aef463b97c70ec96fea25c086eb420e89021a09a
Michelle Chen <mich.chen.94@gmail.com>
2020-10-12 23:28:18 -0700

    added 2-pointer solution

--- two-sum.py
+++ two-sum.py
@@ -2,6 +2,9 @@ from typing import List
 
 class Solution:
 
+    """
+    # ***** Hashmap solution *****
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         indices = {}
@@ -11,6 +14,31 @@ class Solution:
                 return [indices[remaining], i]
             indices[value] = i
         return []
+    """
+
+    # ***** 2-pointer solution *****
+    # must return indices from the original nums array
+        
+    # tuples of (index, element), sorted by element
+    # as if we have sorted list of nums, but need to still keep track of original index
+        # turn enumerate object to list --> elements are (index, num)
+        # key for sorted() will sort by the num element
+    nums_sorted = sorted(list(enumerate(nums)), key=lambda x: x[1])
+    # initialize pointers to beginning and end of array
+    start = 0
+    end = len(nums) - 1
+    
+    # looking at two numbers, so can ignore the last index
+    while start < end:
+        # index into element, if sums == target, return the original index
+        sums = nums_sorted[start][1] + nums_sorted[end][1]
+        if sums == target:
+            return nums_sorted[start][0], nums_sorted[end][0]
+        # move pointers if sum is not target
+        if sums < target:
+            start += 1
+        if sums > target:
+            end -= 1
 
 
 if __name__ == '__main__':
